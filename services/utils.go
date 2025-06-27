03b6a4523118a8f6f9b1e27612ea85eb7a39f239
Yallamaztar <budi.world@yahoo.com>
2025-06-27 07:31:10 +0300

    lol

--- services/utils.go
+++ services/utils.go
@@ -1,1 +1,18 @@
 package services
+
+import (
+	"github.com/Yallamaztar/go-iw4m/services"
+	"github.com/Yallamaztar/go-iw4m/wrapper"
+)
+
+type Utils struct {
+	Wrapper *wrapper.IW4MWrapper
+}
+
+func NewUtils(w *wrapper.IW4MWrapper) *Utils {
+	return &Utils{Wrapper: w}
+}
+
+func DoesRoleExists(role string) bool {
+	server = services.NewServer()
+}
